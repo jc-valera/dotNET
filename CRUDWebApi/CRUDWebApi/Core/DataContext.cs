dd347dfda21f179de74dc887e34e19dd4ab5bf8a
jc-valera <jcvalera87@gmail.com>
2024-01-31 17:27:54 -0600

    We create a basic CRUD API rest usign Entity Framework.

---
+++ CRUDWebApi/CRUDWebApi/Core/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUDWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace CRUDWebApi.Core
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ CRUDWebApi/CRUDWebApi/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUDWebApi.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
