f09455701d8493c38390261b4a97eeef915a729f
jc-valera <jcvalera87@gmail.com>
2024-02-04 12:52:22 -0600

    New Project is load to repository

---
+++ CrudWebApiEntity/Jcvalera.Common/Jcvalera.Common.DataAccess.SqlServer/SqlServerDAL.cs
@@ -0,0 +1,15 @@
+﻿using Jcvalera.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jcvalera.Common.DataAccess.SqlServer
+{
+    public class SqlServerDAL : DbContext
+    {
+        public SqlServerDAL(DbContextOptions<SqlServerDAL> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
