ba8642101a0d86f06f77ee46260c32e95f9542f1
jc-valera <jcvalera87@gmail.com>
2024-02-07 12:47:30 -0600

    New directory is load. CRUD usign ADO

---
+++ CrudWebApiADO/Jcvalera.Common/Jcvalera.Common.Core.BLL/UserBLL.cs
@@ -0,0 +1,49 @@
+﻿using Jcvalera.Common.Core.DAL;
+using Jcvalera.Common.Entities;
+
+namespace Jcvalera.Common.Core.BLL
+{
+    public class UserBLL
+    {
+        public SqlServerDAL dataAccess;
+
+        public UserBLL()
+        {
+            dataAccess = new SqlServerDAL();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await dataAccess.GetUsers();
+
+            return users;
+        }
+
+        public async Task<IEnumerable<User>> CreateUser(User user)
+        {
+            await dataAccess.CreateUser(user);
+
+            var users = await dataAccess.GetUsers();
+
+            return users;
+        }
+
+        public async Task<IEnumerable<User>> UpdateUser(int id, User user)
+        {
+            await dataAccess.UpdateUser(id, user);
+
+            var users = await dataAccess.GetUsers();
+
+            return users;
+        }
+
+        public async Task<IEnumerable<User>> DeleteUser(int id)
+        {
+            await dataAccess.DeleteUser(id);
+
+            var users = await dataAccess.GetUsers();
+
+            return users;
+        }
+    }
+}
---
+++ CrudWebApiADO/Jcvalera.Common/Jcvalera.Common/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Jcvalera.Common.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
