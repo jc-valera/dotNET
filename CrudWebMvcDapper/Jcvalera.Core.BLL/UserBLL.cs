d4384c832877426ace15ca6ddd178ff673a905c2
jc-valera <jcvalera87@gmail.com>
2024-03-03 22:14:24 -0600

    New Prjet is load to the reposotiry MVC using dapper

---
+++ CrudWebMvcDapper/Jcvalera.Core.BLL/UserBLL.cs
@@ -0,0 +1,49 @@
+﻿using Jcvalera.Core.Common.Entities;
+using Jcvalera.Core.Common.Services;
+using Jcvalera.Core.DAL;
+
+namespace Jcvalera.Core.BLL
+{
+    public class UserBLL : IUserBLL
+    {
+        public readonly SqlConfiguration Configuration; 
+        public IUserDAL UserDAL;
+
+        public UserBLL(SqlConfiguration configuration)
+        {
+            Configuration = configuration;
+            UserDAL = new UserDAL(configuration);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            var users = await UserDAL.GetAllUsers();
+
+            return users;
+        }
+
+        public async Task CreateUser(User user)
+        {
+            await UserDAL.CreateUser(user);
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            await UserDAL.DeleteUser(id);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = new User();
+
+            user = await UserDAL.GetUser(id);
+
+            return user;
+        }
+
+        public async Task UpdateUser(int id, User user)
+        {
+            await UserDAL.UpdateUser(id, user);
+        }
+    }
+}
---
+++ CrudWebMvcDapper/Jcvalera.Core.Common/Services/IUserBLL.cs
@@ -0,0 +1,22 @@
+﻿using Jcvalera.Core.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jcvalera.Core.Common.Services
+{
+    public interface IUserBLL
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+
+        Task<User> GetUser(int id);
+
+        Task CreateUser(User user);
+
+        Task UpdateUser(int id, User user);
+
+        Task DeleteUser(int id);
+    }
+}
