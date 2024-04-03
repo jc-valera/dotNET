a8a8459b6e70f9cba121214edea9326862a808bb
jc-valera <jcvalera87@gmail.com>
2024-04-03 11:20:40 -0600

    Ne wporject dot net using JWT is load to the repositoty

---
+++ AuthJasonWebToken/Auth.Core.BLL/RequestBLL.cs
@@ -0,0 +1,46 @@
+﻿using Auth.Core.Common.Entities;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Auth.Core.BLL
+{
+    public class RequestBLL
+    {
+        public string ServiceUrlApi { get; set; }
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            var employees = new List<Employee>();
+
+            try
+            {
+                await GetUrlApi();
+                var url = $"{ServiceUrlApi}/getAllEmployees";
+
+                HttpResponseMessage response;
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+
+                    response = await client.GetAsync(url).ConfigureAwait(false);
+
+                    employees = JsonConvert.DeserializeObject<List<Employee>>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Se produjo un error al obtener la lista de los empleados, intentelo más tarde.");
+            }
+
+
+            return employees;
+        }
+
+        public async Task GetUrlApi()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            ServiceUrlApi = config.GetValue<string>("AppConfig:TestSolApi");
+        }
+    }
+}
---
+++ AuthJasonWebToken/Auth.Core.Common/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth.Core.Common.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstSurname { get; set; }
+
+        public string SecondSurname { get; set; }
+
+        public string Area { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [RegularExpression("([1-9.][0-9.]*)", ErrorMessage = "El campo salario solo se aceptan numeros")]
+        public double Salary { get; set; }
+    }
+}
