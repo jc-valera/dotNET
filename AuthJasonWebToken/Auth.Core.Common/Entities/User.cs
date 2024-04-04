ea29a6a88f83cf035f7b0eb6ff76d7794d45ec14
jc-valera <jcvalera87@gmail.com>
2024-04-04 14:11:51 -0600

    New updates are made in the method authentication controller, using a model to homologate the response in every request to auth method.

--- AuthJasonWebToken/Auth.Core.Common/Entities/User.cs
+++ AuthJasonWebToken/Auth.Core.Common/Entities/User.cs
@@ -8,6 +8,8 @@ namespace Auth.Core.Common.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
--- AuthJasonWebToken/AuthJasonWebToken/Controllers/AuthenticationController.cs
+++ AuthJasonWebToken/AuthJasonWebToken/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Auth.Core.Common.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -11,11 +12,23 @@ namespace AuthJasonWebToken.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        public string SecretKey;
+        //public string SecretKey;
 
-        public AuthenticationController(IConfiguration config)
+        //public AuthenticationController(IConfiguration config)
+        //{
+        //    SecretKey = config.GetSection("AppSettings").GetSection("SecretKey").ToString();
+        //}
+        //private readonly string SecretKey;
+        //public AuthenticationController(IConfiguration config)
+        //{
+        //    SecretKey = config.GetSection("AppSettings").GetSection("SecretKey").ToString();
+        //}
+
+        public AppSettings AppSettings;
+
+        public AuthenticationController(IOptions<AppSettings> appSettings)
         {
-            SecretKey = config.GetSection("AppSettings").GetSection("SecretKey").ToString();
+            AppSettings = appSettings.Value;
         }
 
         [HttpPost]
@@ -24,7 +37,7 @@ namespace AuthJasonWebToken.Controllers
         {
             if (user.Email == "jcvalera.emp@gmail.com" && user.Password == "123456")
             {
-                var keyBytes = Encoding.ASCII.GetBytes(SecretKey);
+                var keyBytes = Encoding.ASCII.GetBytes(AppSettings.SecretKey);
                 var claims = new ClaimsIdentity();
 
                 claims.AddClaim(
@@ -51,6 +64,55 @@ namespace AuthJasonWebToken.Controllers
             {
                 return StatusCode(StatusCodes.Status401Unauthorized, new { token = "" });
             }
+        }
+
+        [HttpPost]
+        [Route("validateAuth1")]
+        public async Task<IActionResult> ValidateAuth1([FromBody] User user)
+        {
+            AuthResponse authResponse;
+
+            if (user.Email == "jcvalera.emp@gmail.com" && user.Password == "123456")
+            {
+                var keyBytes = Encoding.ASCII.GetBytes(AppSettings.SecretKey);
+
+                var claims = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                        new Claim(ClaimTypes.Email, user.Email)
+                    });
+
+                var tokenDescriptior = new SecurityTokenDescriptor
+                {
+                    Subject = claims,
+                    Expires = DateTime.UtcNow.AddMinutes(60),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenConfig = tokenHandler.CreateToken(tokenDescriptior);
+
+                authResponse = new AuthResponse
+                {
+                    Token = tokenHandler.WriteToken(tokenConfig),
+                    Message = "Token generado correctamente.",
+                    Success = true,
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            else
+            {
+                authResponse = new AuthResponse
+                {
+                    Token = "",
+                    Message = "Usuario o Contraseña incorrecta.",
+                    Success = false,
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+
+            return Ok(authResponse);
         }
     }
 }
