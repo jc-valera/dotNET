5dfc4895c2753e91a856e8d9e0b3006c6685ca53
jc-valera <jcvalera87@gmail.com>
2024-04-08 22:52:07 -0600

    New project is load to the repository, in this repositori we test some way to validate files from api post request.

---
+++ WaysValidateUploadedFiles/WaysValidateUploadedFiles/Controllers/UploadController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WaysValidateUploadedFiles.Core.BLL;
+
+namespace WaysValidateUploadedFiles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("byExtension")]
+        public async Task<IActionResult> UploadFileExtension(IFormFile file)
+        {
+            var validateClass = new FileValidatorBLL();
+
+            if (!validateClass.IsFileCorrect(file))
+                return BadRequest("El archivo es incorrecto.");
+
+            string[] allowedExtensions = [".pdf", ".doc", ".docx"];
+
+            if (!validateClass.IsFileExtensionAllowed(file, allowedExtensions))
+                return BadRequest("La extension del archivo es incorrecto");
+
+            return Ok();
+        }
+
+        [HttpPost("bySize")]
+        [RequestSizeLimit(1_000_000)] // Checking for 1 MB
+        public async Task<IActionResult> UploadFileBySize(IFormFile file)
+        {
+            var validateClass = new FileValidatorBLL();
+
+            if (!validateClass.IsFileCorrect(file))
+                return BadRequest("El archivo es incorrecto.");
+
+            var size = 1024 * 1024;
+
+            if (!validateClass.IsFileSizeWithinLimit(file, size))
+                return BadRequest("El tamaño del archivo excede el tamaño permitido (1 MB)");
+
+            return Ok();
+        }
+
+        [HttpPost("byName")]
+        public async Task<IActionResult> UploadFileName(IFormFile file)
+        {
+            var validateClass = new FileValidatorBLL();
+
+            if (!validateClass.IsFileCorrect(file))
+                return BadRequest("El archivo es incorrecto.");
+
+            if (validateClass.IsFileWithSameName(file))
+                return BadRequest("Documento con mismo nombre, Por favor cargue un documento con diferente nombre.");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [FileValidationFilter([".pdf", ".doc", ".docx"], 1024 * 1024)]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var fileValidator = new FileValidatorBLL();
+
+            await fileValidator.SaveDocument(file);
+
+            return Ok();
+        }
+
+    }
+
+}
---
+++ WaysValidateUploadedFiles/WaysValidateUploadedFiles/Core/BLL/FileValidator.cs
@@ -0,0 +1,44 @@
+﻿namespace WaysValidateUploadedFiles.Core.BLL
+{
+    public static class FileValidator
+    {
+        public static bool IsFileCorrect(IFormFile file)
+        {
+            bool isFileCorrect = true;
+
+            if (file is null || file.Length == 0)
+                isFileCorrect = false;
+
+            return isFileCorrect;
+        }
+
+        public static bool IsFileExtensionAllowed(IFormFile file, string[] allowedExtensions)
+        {
+            var extension = Path.GetExtension(file.FileName);
+
+            var extensionIsAllowed = allowedExtensions.Contains(extension);
+
+            return extensionIsAllowed;
+        }
+
+        public static bool IsFileSizeWithinLimit(IFormFile file, long maxSizeInBytes)
+        {
+            var length = file.Length <= maxSizeInBytes;
+
+            return length;
+        }
+
+        public static bool IsFileWithSameName(IFormFile file)
+        {
+            var fileExist = false;
+            var logicGetFileName = "CV_Carlos_Valera_mx";
+
+            var currentFileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+            if (currentFileName == logicGetFileName)
+                fileExist = true;
+
+            return fileExist;
+        }
+    }
+}
