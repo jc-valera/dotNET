c3613394850f30d4f2cb761cb06477d5d1f57a3f
jc-valera <jcvalera87@gmail.com>
2024-04-15 22:35:54 -0600

    New project using WCF to test programan is load to the repositori.

---
+++ WCFService/Wcf.Core.BLL/PersonBLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wcf.Core.Common.Entities;
+using Wcf.Core.DAL;
+
+namespace Wcf.Core.BLL
+{
+    public class PersonBLL
+    {
+        public PersonDAL PersonDAL;
+
+        public PersonBLL()
+        {
+            PersonDAL = new PersonDAL();
+        }
+
+        public async Task<List<Person>> GetPersons()
+        {
+            var persons = new List<Person>();
+
+            persons = await PersonDAL.GetPersons();
+
+            return persons;
+        }
+    }
+}
